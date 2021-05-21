c543f90f6bb853ebd6a3310b484c3fa130f9de8d
Adithya Vijay K <adithyavj@gmail.com>
2021-05-21 20:31:24 +0530

    Add new controller,view
    Added a new controller called PeopleController
    with default action Index(). Created a view for
    this controller called index.cshtml in the people
    folder in Views Folder

--- MVCDemo/App_Start/RouteConfig.cs
+++ MVCDemo/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace MVCDemo
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // This is where the Routes are specified/configured.
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // Default for a controller is index..
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
